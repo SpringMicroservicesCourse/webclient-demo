1c2f4377cc8531a38d0a710b171390c6f093df22
chukuolung <chukuo99@gmail.com>
2025-07-25 16:26:14 +0800

    init

---
+++ src/test/java/tw/fengqing/spring/reactor/webclient/WebclientDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package tw.feingqing.spring.reactor.webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class WebclientDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
